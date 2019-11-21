d549d1e70a5bee7fb06e98e4b7ff6ca9d4f2277e
ucipass@gmail.com <G9resume1>
2019-11-21 16:36:08 -0600

    build

--- test-local.js
+++ test-local.js
@@ -5,8 +5,9 @@ async function run(){
     let client1 = new Client("client1");
     let client2 = new Client("client2");
 
+    // comment out whicever component needed test
     await server.started;
-    // await client1.start()
+    await client1.start()
     await client2.start()
 }
 
