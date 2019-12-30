043fe727df0340772a3638ae3bad523887e2cce5
ucipass <ucipass@gmail.com>
2019-12-30 07:28:20 +0100

    git

--- manager/vue.config.js
+++ manager/vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   configureWebpack: { 
     devtool: 'source-map' 
   },
-  publicPath: process.env.VUE_APP_PREFIX
+  // publicPath: process.env.VUE_APP_PREFIX
+  publicPath: ""
 }
