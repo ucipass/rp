2ecc5d053d83d087a4646f01c319e118f99445ad
ucipass@gmail.com <G9resume1>
2020-02-08 18:58:26 -0600

    auth

--- mongooseclient.js
+++ mongooseclient.js
@@ -1,4 +1,6 @@
 "use strict";
+var log = require("ucipass-logger")("mongooseclient")
+log.transports.console.level = 'info'
 const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true);
 const DATABASE_URL      = process.env.DATABASE_URL
@@ -6,32 +8,50 @@ const DATABASE_USERNAME = process.env.DATABASE_USERNAME
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
 const CollectionClients = 'Clients'
 const CollectionWebusers = 'Webusers'
+const ClientSchema = new mongoose.Schema({
+    client: { type: String, required: true, unique: true },
+    token: { type: String, required: true },
+    ipaddr: { type: Date, required: false, default: Date.now },
+    expiration: { type: Date, required: true, default: Date.now },
+});
+const WebuserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    expiration: { type: Date, required: true, default: Date.now },
+});
+let options ={
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    user: DATABASE_USERNAME,
+    pass: DATABASE_PASSWORD,                  
+    auth:{
+        authSource: 'admin'                    
+    }
+}
+// CHECK IF ADMIN USER EXISTS IF NOT CREATE admin/admin
+const connection  = mongoose.createConnection( DATABASE_URL, options);
+const Webuser = connection.model( CollectionWebusers, WebuserSchema)
+Webuser.findOne({ "username" : "admin" })
+.then((user)=>{
+    if(user){
+        return true
+    }else{
+        let doc = new Webuser ({username: "admin", password: "admin"})
+        log.info("Creating new admin user account!")
+        return doc.save()  
+    }
+})
+.catch((error)=>{
+    log.error("Mongo DB error with admin user id",error)
+})
+
+
 
 module.exports = function(){
-    let options ={
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        user: DATABASE_USERNAME,
-        pass: DATABASE_PASSWORD,                  
-        auth:{
-            authSource: 'admin'                    
-        }
-    }
+
 
     const connection  = mongoose.createConnection( DATABASE_URL, options);
-    const ClientSchema = new mongoose.Schema({
-        client: { type: String, required: true, unique: true },
-        token: { type: String, required: true },
-        ipaddr: { type: Date, required: false, default: Date.now },
-        expiration: { type: Date, required: true, default: Date.now },
-    });
-    const Client = connection.model( CollectionClients, ClientSchema)
-
-    const WebuserSchema = new mongoose.Schema({
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        expiration: { type: Date, required: true, default: Date.now },
-    });
+    const Client  = connection.model( CollectionClients , ClientSchema )
     const Webuser = connection.model( CollectionWebusers, WebuserSchema)
 
     connection.createClient = async (client)=>{
