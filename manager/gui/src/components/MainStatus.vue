1f7c50667f9da340cd6b6f3d740977ca66ef27fb
ucipass@gmail.com <G9resume1>
2020-03-25 12:52:46 -0500

    status update

--- manager/gui/src/components/MainStatus.vue
+++ manager/gui/src/components/MainStatus.vue
@@ -1,9 +1,29 @@
 <template>
 
   <b-container fluid v-if='showMainStatus'>
-    <h1>MainStatus</h1>    
+    <h3>Clients Authenticated: {{ statusdata.clients_authenticated }} </h3>
     <div class="text-center">
-      {{statusjson}}
+      <!-- {{statusjson}} -->
+      <b-card v-for="client in clients" :key="client.name" no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block href="#" v-b-toggle="client.name" variant="info">{{client.name}}</b-button>
+        </b-card-header>
+        <b-collapse v-bind:id="client.name" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text v-for="prop in Object.keys(client)" :key=prop >
+              <div class="row">
+                <div class="col text-right">
+                  {{ prop }}:
+                </div>
+                <div class="col text-left">
+                  {{ client[prop] }}
+                </div>
+              </div>                
+              <!-- {{ JSON.stringify(prop) }} {{ JSON.stringify(client[prop]) }} -->
+            </b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card>      
     </div>
   </b-container>
 </template>
@@ -16,8 +36,12 @@ import {  eventBus, hideMainAll } from './events.js'
 export default {
   name: "App",
   data: () => ({
+    test:1,
     showMainStatus: false,
-    statusjson: "Waiting for data from server..."
+    statusjson: "Waiting for data from server...",
+    text: "Waiting for data from server...",
+    clients: [ {name:"c1",token: 1},{name:"c2",token: 2}],
+    statusdata: null
     // status: null
   }),
   mounted: async function () {
@@ -39,6 +63,8 @@ export default {
     status: async function(){
       axios.get(URL_STATUS)
       .then( response =>{
+        this.statusdata = response.data
+        this.clients = response.data.clients
         this.statusjson = JSON.stringify(response.data, null, 2);
         console.log(this.statusjson)
       })
